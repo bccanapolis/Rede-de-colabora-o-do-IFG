b11642d670af77b8be9c3ef6e4cdf6c609c028f7
Bruno de Araujo Alves <me@baraus.dev>
2026-07-17 15:06:56 -0300

    feat: adiciona healthcheck ao backend

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+
+def health_check(request):
+    return JsonResponse({"status": "ok"})
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("apps.network.urls")),
+    path("health/", health_check),
 ]
